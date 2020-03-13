341cb88764acba8724d4a1bba301b1f53cbc6344
gabriel <santos.gabrieljesus@outlook.com>
2020-03-12 21:50:31 -0300

    Initial Commit

---
+++ MyGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGames
+//
+//  Created by Gabriel Jesus Santos  on 12/03/20.
+//  Copyright © 2020 Self Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
